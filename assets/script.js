37f9cba53584c87b630d53d8a854f8616c55c55e
Kevin Eduardo <kevineduardomanager@gmail.com>
2021-08-09 15:14:47 -0300

    Melhoras na  responsividade

---
+++ assets/script.js
@@ -0,0 +1,48 @@
+
+   let iconMenu= document.getElementById("check");
+   let larguraJanela= window.innerWidth;
+   let alturaJanela= window.innerHeight;
+   let menu= document.getElementById('links');
+   var subitens=menu.children;
+
+   function ExibirSubItensMenu(id){
+     
+      let menu = document.getElementById(id);
+      let submenu = menu.children;
+      let icon_dark=submenu[1].getElementsByClassName('icon-arrow');
+     
+      if (submenu[2].style.display == 'none') {   
+         submenu[2].style.display='block';
+        
+         icon_dark[0].style.transform='rotate(180deg)';   
+      }else {
+         icon_dark[0].style.transform='rotate(0deg)';
+         submenu[2].style.display='none';      
+      }
+   }
+
+
+   function alterarIcon(event) {
+      let iconMenu=document.getElementById('iconMenu');
+      if(event.target.checked){
+         iconMenu.src='../images/icon-close.svg';
+      }else{
+         iconMenu.src='../images/icon-hamburger.svg';
+      }
+   }
+   
+   function Eventos() {
+      let i=0;
+     do{
+        if( larguraJanela<=660 ){
+            subitens[i].addEventListener('click',function (event) { 
+                  let item = event.target;
+                  ExibirSubItensMenu(item.id);
+                  })                   
+        }
+     
+      i++; 
+          }while(i<=2);
+      }
+
+   Eventos();
